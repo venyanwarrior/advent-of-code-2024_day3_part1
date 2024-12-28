cbd48d03d70ea316b7bc7aaecbe9b937dbfc20ea
Spencer Williams <swilliams94@go.pasadena.edu>
2024-12-27 23:04:58 -0800

    init

---
+++ Part1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <iterator>
+#include <vector>
+#include <cstdio>
+
+int main() {
+    std::ifstream infile;
+    infile.open("../input.txt");
+    if (!infile.is_open()) {
+        std::cout << "Input file opening failed." << std::endl;
+        exit(1);
+    }
+
+    std::string inputData;
+    std::string dataChunk;
+    while (!infile.eof()) {
+        std::getline(infile, dataChunk);
+        inputData += dataChunk;
+    }
+
+    std::regex mulRegex(R"rgx(mul\(\d+,\d+\))rgx");
+    auto mulBegin = std::sregex_iterator(inputData.begin(), inputData.end(), mulRegex);
+
+    std::cout << "Found " << std::distance(mulBegin, std::sregex_iterator()) << " occurences of mul(x,x):\n";
+
+    std::vector<std::string> mulMatches;
+    for (auto it = mulBegin; it != std::sregex_iterator(); it++) {
+        std::smatch mulMatch = *it;
+        mulMatches.push_back(mulMatch.str());
+    }
+
+    for (std::string s : mulMatches) {
+        std::cout << s << std::endl;
+    }
+
+    std::regex numRegex(R"rgx(\d+)rgx");
+    std::sregex_iterator numBegin;
+
+    int total = 0;
+    for (auto vIt = mulMatches.begin(); vIt != mulMatches.end(); vIt++) {
+        std::string mulString = std::data(*vIt);
+        numBegin = std::sregex_iterator(mulString.begin(), mulString.end(), numRegex);
+        for (auto sIt = numBegin; sIt != std::sregex_iterator(); sIt++) {
+            std::smatch num1 = *sIt;
+            sIt++;
+            std::smatch num2 = *sIt;
+            total += stoi(num1.str()) * stoi(num2.str());
+            printf("num1: %s, num2: %s\n", num1.str().c_str(), num2.str().c_str());
+        }
+    }
+    printf("Total: %d\n", total);
+}
